a4574e9ffc140cbae973504a17e39c03ff1ab0df
Gino Carignano <gino.jc@hotmail.com>
2019-09-19 23:17:51 -0300

    Avance con Redux, Store, Reducers y Containers

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
 import React, {Component} from 'react';
 import {Grid, Row, Col} from 'react-flexbox-grid';
-import {createStore} from 'redux';
 import Paper from 'material-ui/Paper';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
-import LocationList from './components/LocationList';
-import ForecastExtended from './components/ForecastExtended';
+import LocationListContainer from './containers/LocationListContainer';
+import ForecastExtendedContainer from './containers/ForecastExtendedContainer';
+
+
 import './App.css';
 
-const cities= [
+const cities= [ 
   'Buenos Aires,ar',
   'Córdoba,ar',
   'Madrid,es',
   'Ciudad de México,mx'
 ];
 
-const store = createStore(() => {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {city: null};
-  }
-
-  handleSelectedLocation = city => {
-    this.setState({city});
-
-    const action = {type: 'setCity', value: city};
-    store.dispatch(action);
-  }
-
   render(){
-    const {city} = this.state;
     return (
 
       <MuiThemeProvider>
@@ -45,16 +30,12 @@ class App extends Component {
           </Row>
           <Row>
             <Col xs={12} md={6}>
-            <LocationList cities={cities}
-              onSelectedLocation={this.handleSelectedLocation}></LocationList>
+            <LocationListContainer cities={cities}></LocationListContainer>
             </Col>
             <Col xs={12} md={6}>
                 <Paper zDepth={4}>
                 <div className='detail'>
-                  {
-                    city &&
-                      <ForecastExtended city={city}></ForecastExtended>
-                  }
+                  <ForecastExtendedContainer></ForecastExtendedContainer>
                 </div>
                 </Paper>
             </Col>

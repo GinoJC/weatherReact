1fd3a4960ccc568af45f2a052fba2efb38ba0dea
Gino Carignano <gino.jc@hotmail.com>
2019-09-21 17:21:46 -0300

    redux Avanzado, Middleware thunk, refactor, selector

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,73 +1,34 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import ForecastItem from './ForecastItem';
-import transformForecast from './../services/transformForecast';
+import ForecastItem from './ForecastItem'; 
 import './styles.css';
 
-const url = "http://api.openweathermap.org/data/2.5/forecast";
-const api_key = "18dfe2ca8944bf44a023e6d2cefadca9";
-
-class ForecastExtended extends Component {
-
-    constructor(){
-        super();
-        this.state = { forecastData: null }
-    }
-
-    componentDidMount(){
-        //fetch
-        this.updateCity(this.props.city);
-    }
-
-    componentWillReceiveProps(nextProps){
-        if(nextProps.city !== this.props.city){
-            this.setState({forecastData: null});
-            this.updateCity(nextProps.city);
-        }
-    }
-
-    updateCity = city => {
-        const url_forecast = `${url}?q=${city}&APPID=${api_key}`;
-        
-        fetch(url_forecast).then(
-            data => (data.json())
-        ).then(
-            weather_data => {
-                const forecastData = transformForecast(weather_data);
-                this.setState({forecastData});
-            }
-        );
-    }
-
-    renderForecastItemDays(forecastData){
-        return forecastData.map(forecast => (
-            <ForecastItem 
-                key={`${forecast.weekDay}${forecast.hour}`}
-                weekDay={forecast.weekDay} 
-                hour={forecast.hour}
-                data={forecast.data}>
-            </ForecastItem>));
-    }
-
-    renderProgress = () => {
-        return <h3>Cargando Pronóstico Extendido...</h3>;
-    }
-
-    render() {
-        const {city} = this.props;
-        const {forecastData} = this.state;
-        return(
-            <div>
-                <h2 className='forecast-title'>Pronóstico Extendido para {city}</h2>
-                {forecastData ?
-                    this.renderForecastItemDays(forecastData) :
-                    this.renderProgress()}
-            </div>);
-    }
+const renderForecastItemDays = forecastData => {
+    return forecastData.map(forecast => (
+        <ForecastItem 
+            key={`${forecast.weekDay}${forecast.hour}`}
+            weekDay={forecast.weekDay} 
+            hour={forecast.hour}
+            data={forecast.data}>
+        </ForecastItem>));
 }
+
+const renderProgress = () => {
+    return <h3>Cargando Pronóstico Extendido...</h3>;
+}
+
+const ForecastExtended = ({city, forecastData}) => (
+    <div>
+        <h2 className='forecast-title'>Pronóstico Extendido para {city}</h2>
+        {forecastData ?
+            renderForecastItemDays(forecastData) :
+            renderProgress()}
+    </div> 
+);
 
 ForecastExtended.propTypes = {
     city: PropTypes.string.isRequired,
+    forecastData: PropTypes.array,
 }
 
 export default ForecastExtended;
---
+++ src/reducers/cities.js
@@ -0,0 +1,16 @@
+import { createSelector } from 'reselect';
+import { SET_FORECAST_DATA } from './../actions';
+
+export const cities = (state = {}, action) => {
+    switch (action.type) {
+        case SET_FORECAST_DATA:
+            // eslint-disable-next-line no-case-declarations
+            const {city, forecastData} = action.payload;
+            return { ...state, [city]: {forecastData}};
+        default:
+            return state;
+    }
+}
+
+export const getForecastDataFromCities =
+    createSelector((state, city) => state[city] && state[city].forecastData, forecastData => forecastData);
